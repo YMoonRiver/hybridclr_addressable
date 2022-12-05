5c7fcfa1b743ddd77a1d565052c0e2fb3605f5b6
yellow <82157476@qq.com>
2022-12-05 19:11:03 +0800

    build editor

--- Assets/HotFix/HotUpdateMain.cs
+++ Assets/HotFix/HotUpdateMain.cs
@@ -1,6 +1,7 @@
 using HybridCLR;
 using System.Collections;
 using System.Collections.Generic;
+using UniRx;
 using UnityEngine;
 using Wanderer.GameFramework;
 
@@ -17,7 +18,7 @@ public class HotUpdateMain : MonoBehaviour
 
         gameObject.AddComponent<CreateByCode>();
 
-        Debug.Log("=======看到此条日志代表你成功运行了示例项目的热更新代码=======123456");
+        Debug.Log("=======看到此条日志代表你成功运行了示例项目的热更新代码=======");
 
         //开始游戏
         Debug.Log("PlayStateContext Begin");
@@ -30,5 +31,28 @@ public class HotUpdateMain : MonoBehaviour
             };
         GameMode.FSM.GetFSM<PlayStateContext>().Creat(states);
         GameMode.FSM.GetFSM<PlayStateContext>().OnBegin();
+
+        Observable.Timer(System.TimeSpan.FromSeconds(2))
+            .Subscribe(_ => { Debug.Log("delay 2 seconds"); });
+
+        Observable.EveryUpdate()
+                .Where(_ => Input.GetMouseButtonDown(0))
+                .Subscribe(_ => {
+
+                    Debug.Log("鼠标点击");
+                });
+
+        Observable.EveryUpdate().
+                Where(_ => Input.GetMouseButtonDown(0)) //相当于if 判断
+                .First() //只执行一次  Subscribe：订阅的事件或者回调
+                .Subscribe(_ => {
+                    Debug.Log("鼠标只能点击一次");
+
+                }).AddTo(this);
+
+        Observable.EveryUpdate().Where(_ => Input.GetMouseButtonDown(0))
+                    .Throttle(System.TimeSpan.FromSeconds(1))
+                    .Subscribe(_ => Debug.Log("一秒过后"));
     }
+
 }
